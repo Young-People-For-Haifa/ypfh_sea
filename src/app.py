3a070a38216b87081b086bd92f5cf58a9d0a9089
Roman Kudryavtsev <kudryavtsevroman@mail.ru>
2024-07-01 19:44:34 +0300

    refactor lang selection

--- src/app.py
+++ src/app.py
@@ -1,33 +1,42 @@
 import os
-from flask import Flask, render_template, request
-from flask_babel import Babel
-from src.locale import Config, default, en, he
-
-
-def get_locale():
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
-
+from flask import Flask, render_template, request, session, g
+from src.locales import ru, en, he
 
 app = Flask(__name__)
 app.static_folder = "./static"
 app.secret_key = os.environ.get("SECRET_KEY")
-app.config.from_object(Config)
-babel = Babel(app, locale_selector=get_locale)
+
+
+def _get_locale(lang_code: str):
+    match lang_code:
+        case "ru":
+            return ru
+        case "en":
+            return en
+        case "he":
+            return he
+        case _:
+            return ru
+
+
+@app.before_request
+def get_locale_from_session():
+    if "lang" in session:
+        lang_code = session.get("lang")
+        g.locale = _get_locale(lang_code)
+        g.lang = lang_code
 
 
 @app.route("/")
 def home():
-    lang_code = request.args.get("lang", default=None)
-    if lang_code:
-        match lang_code:
-            case "ru":
-                locale = default
-            case "en":
-                locale = en
-            case "he":
-                locale = he
-            case _:
-                locale = default
+    locale = g.get("locale", None)
+    lang = g.get("lang", None)
+    selected_lang = request.args.get("lang", default=None)
+    if locale and lang and not selected_lang:
+        return render_template("index.html", locale=locale, lang=lang)
+    elif selected_lang:
+        selected_locale = _get_locale(selected_lang)
+        session["lang"] = selected_lang
+        return render_template("index.html", locale=selected_locale, lang=selected_lang)
     else:
-        locale = default
-    return render_template("index.html", locale=locale, lang=lang_code)
+        return render_template("index.html", locale=ru, lang="ru")
